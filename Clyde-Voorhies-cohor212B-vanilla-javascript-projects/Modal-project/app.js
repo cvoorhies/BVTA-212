0f211f73954e356af090ef3aab795a4946e76943
cvoorhies <MyOfficeSpace888@gmail.com>
2021-05-23 05:43:56 -0700

    making sure all files updated and pushed

--- Clyde-Voorhies-cohor212B-vanilla-javascript-projects/Modal-project/app.js
+++ Clyde-Voorhies-cohor212B-vanilla-javascript-projects/Modal-project/app.js
@@ -1,4 +1,6 @@
 // select modal-btn,modal-overlay,close-btn
+<btn>OPEN MODAL</btn>
 // listen for click events on modal-btn and close-btn
+window.eventListener('click', )
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
